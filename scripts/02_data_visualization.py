5e31f6491a99c31a21400c64b8f4cc9e1ddbf528
Benjamin Chan <benjamin.v.chan@gmail.com>
2025-01-08 23:36:15 -0800

    02 data visualization script outline

---
+++ scripts/02_data_visualization.py
@@ -0,0 +1,14 @@
+# import:
+    # matplotlib - for data vis
+    # pandas - for df to manipulate and use data
+
+# Helper func
+    # load files as pandas df
+    # load plot results func
+
+# Main func
+# Load data
+    # Compare switch VS no switch results
+        # Use iteration between both categories
+    # use bar chart
+# Plot results
