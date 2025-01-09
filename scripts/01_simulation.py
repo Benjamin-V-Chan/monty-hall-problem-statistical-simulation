ca4392e4f3ee65da7a6386f7c2f1d0c0b0c6fe49
Benjamin Chan <benjamin.v.chan@gmail.com>
2025-01-08 23:35:05 -0800

    01 simulation scripts outline

---
+++ scripts/01_simulation.py
@@ -0,0 +1,11 @@
+# import:
+    # random - for simulation logic
+    # csv - for saving data
+    # os - for file manipulation
+
+# Ensure the outputs folder exists
+# Create simulation func
+# Create main loop
+    # Run simulation func
+# Save results to txt
+# Print log each step
